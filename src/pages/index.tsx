cb774a006ad585fb85932fb5041e7422285529cc
sajjadramezani <sajjad.payam77@gmail.com>
2023-03-27 10:59:13 +0330

    feat: add redux to project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head'
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+  const { user } = useSelector((store: any) => store.user);
+  console.log(user)
   return (
     <>
       <Head>
         <title>Create Next App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <div>ssdfsdf</div>
     </>
   )
 }
