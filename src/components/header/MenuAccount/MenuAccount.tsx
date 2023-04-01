0df4c3661e00d5955e9419ff6b9497c478de6129
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-01 15:06:08 +0330

    fix: Show cart length in header

--- src/components/header/MenuAccount/MenuAccount.tsx
+++ src/components/header/MenuAccount/MenuAccount.tsx
@@ -1,12 +1,21 @@
+import { useCartActions } from '@/src/store/cartSlice'
 import LoadSvgIcon from '@/src/utils/LoadSvgIcon'
 
 const MenuAccount = () => {
+
+    const { cartQty } = useCartActions()
+
     return (
         <div className="flex">
             <span className="rounded-full p-3 hover:bg-primary/10 duration-300 cursor-pointer">
                 <LoadSvgIcon name="user" weight={1.5} size={22} color="var(--color-titr)" />
             </span>
-            <span className="rounded-full p-3 hover:bg-primary/10 duration-300 cursor-pointer">
+            <span className="rounded-full p-3 hover:bg-primary/10 duration-300 cursor-pointer relative">
+                {
+                    cartQty > 0 && <span className='absolute top-1.5 left-1.5 w-4 h-4 rounded-full flex justify-center items-center text-[10px] text-white bg-red-500'>
+                        {cartQty}
+                    </span>
+                }
                 <LoadSvgIcon name="cart" weight={1.5} size={22} color="var(--color-titr)" />
             </span>
         </div>
