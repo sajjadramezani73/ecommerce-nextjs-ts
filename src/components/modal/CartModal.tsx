5c5279f79d38a80f0fbcafb0706599f73d5b1359
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-03 18:50:09 +0330

    feat: Show cart items in modal cart with horizontalProduct file

--- src/components/modal/CartModal.tsx
+++ src/components/modal/CartModal.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import ModalRoot from './Modal-root'
 import products from '../../data/products.json'
+import HorizontalProduct from '../propduct/horizontalProduct/HorizontalProduct'
 
 
 const CartModal = () => {
@@ -17,7 +18,7 @@ const CartModal = () => {
     return (
         <>
             {showCart && <ModalRoot show={showCart} onClick={() => closeCart()} >
-                <div className={`bg-white h-full w-4/5 min-w-[320px] max-w-[320px]  duration-300 sidebar
+                <div className={`bg-white h-full w-4/5 min-w-[450px] max-w-[450px]  duration-300 sidebar
                 ${showCart ? 'active' : ''}`}
                 >
                     <div className="h-full flex flex-col justify-between rtl">
@@ -27,7 +28,18 @@ const CartModal = () => {
                             </span>
                             <p className='text-sm text-captionDark'>سبد خرید</p>
                         </div>
-                        <div className="flex-grow">product</div>
+                        <div className="flex-grow p-4 flex flex-col gap-y-4 overflow-hidden overflow-y-auto no-scroll">
+                            {cart.length > 0 ? (
+                                cart.map(item => {
+                                    const product = products.find(product => product.id == item.id)!
+                                    return <HorizontalProduct key={product.id} product={product} />
+                                })
+                            ) : (
+                                <div className="h-full flex justify-center items-center">
+                                    <p className='text-sm text-caption'>محصولی به سبد خرید اضافه نشده است</p>
+                                </div>
+                            )}
+                        </div>
                         <div className="flex items-center border-t p-4">
                             <button className='bg-primary rounded h-10 w-1/3 text-xs text-white'>ادامه</button>
                             <div className="w-2/3 text-left">
