17096a8fbfed5083abef92a8db7db0058f36f5a7
sajjadramezani <sajjad.payam77@gmail.com>
2023-04-03 20:13:36 +0330

    fix: Fix bug interface type for axios

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,11 +1,12 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig, AxiosRequestHeaders } from "axios";
 import Cookies from "js-cookie";
 import { baseUrl } from '../constant/index'
+
 const service = axios.create()
 service.defaults.baseURL = baseUrl
 // service.defaults.timeout = 15000;
 service.interceptors.request.use(
-    (config) => {
+    (config: any) => {
         const token = Cookies.get("token");
         config.headers = {
             'Accept': 'application/json',
